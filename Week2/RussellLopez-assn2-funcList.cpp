110b28321a209a030a096ccddf82bff3769236dc
Jason Lopez <jlopez0896@gmail.com>
2012-11-03 23:41:05 -0600

    Add function list for assignment 2 program

---
+++ Week2/RussellLopez-assn2-funcList.cpp
@@ -0,0 +1,241 @@
+//***************************************************************************
+//  FUNCTION: main
+//  DESCRIPTION: entry point for the program, calls to all secondary functions
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT: None
+//  OUTPUT:
+//        Return Val:  0 for successful processing
+//  CALLS TO:
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: InitializeArrays
+//  DESCRIPTION: function initializes all dynamic arrays needed for the program
+//  IMPLEMENTED BY:
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    linearHash - reference array for linear hash order of keys
+//                    doubleHash - reference array for double hash order of keys
+//                    chainingHash - reference array for separate chaining hash
+//                                   order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: DeleteArrays
+//  DESCRIPTION: function delets all dynamic arrays to free memory
+//  IMPLEMENTED BY:
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    linearHash - reference array for linear hash order of keys
+//                    doubleHash - reference array for double hash order of keys
+//                    chainingHash - reference array for separate chaining hash
+//                                   order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: PopulateRandomArray
+//  DESCRIPTION: Calls GenerateNumber to get number. Runs sequential search of
+//               array to test for number if no match found, number is added to
+//               next available space. Repeats until array filled.
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: randArray - reference array to contain all random numbers
+//                    to be used as the key values for the program
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GenerateNumber, SearchArray
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: GenerateNumber
+//  DESCRIPTION: uses cstdlib rand() and the limits of MIN_RAND and MAX_RAND to
+//               generate a random integer and return it to the calling function
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: None
+//  OUTPUT:
+//        Return Val: random integer
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: SearchArray
+//  DESCRIPTION: Sequential search of each member of array for matching number.
+//               Returns false if nothing found, true if match found
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: tempNum - integer to search for
+//                    randArray - reference array to be searched
+//                    arraySize - size of the array to be searched
+//  OUTPUT:
+//        Return Val: bool - true if integer is found, false if integer is not
+//                           found in the array
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: PopulateLinearHash
+//  DESCRIPTION: function populates array with key integers from the random array
+//               by using linear probing collision technique
+//  IMPLEMENTED BY: Jason Lopez
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    linearHash - reference array for linear hash order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: PopulateDoubleHash
+//  DESCRIPTION: function populates array with key integers from the random array
+//               by using double hashing collision technique
+//  IMPLEMENTED BY: Jason Lopez
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    doubleHash - reference array for double hash order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation, GetSecondaryHashLocation
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: PopulateChainingHash
+//  DESCRIPTION: function populates array wtih key integers from the random array
+//               by using separate chaining collision technique
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    chainingHash - reference array for separate chaining hash
+//                                   order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation, InitializeNode
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: GetPrimryHashLocation
+//  DESCRIPTION: function uses modulo-division to determine the initial location
+//               for a key
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: key - integer to be added to a hash table
+//                    listSize - integer size of the hash table
+//  OUTPUT:
+//        Return Val: int - initial position to place the key in the hash table
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: GetSecondaryHashLocation
+//  DESCRIPTION: function uses a secondary hash algorithm to deterimine an
+//               alternate position if a collision is encountered with the primary
+//               hash location
+//  IMPLEMENTED BY: Jason Lopez
+//  INPUT:
+//        Parameters: key - integer to be added to a hash table
+//                    listSize - integer size of the hash table
+//  OUTPUT:
+//        Return Val: int - second position to place the key in the hash table
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: InitializeNode
+//  DESCRIPTION: function initializes a node for adding to the chaining hash
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: chainingNode - node for key integers that had a collision
+//                    with primary hash position
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: SearchLinear
+//  DESCRIPTION: function searches the linear hash array to find half of the
+//               integers in the random array
+//  IMPLEMENTED BY: Jason Lopez
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    linearHash - reference array for linear hash order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation, OutputResults
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: SearchDouble
+//  DESCRIPTION: function searches the double hash array to find half of the
+//               integers in the random array
+//  IMPLEMENTED BY: Jason Lopez
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    doubleHash - reference array for double hash order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation, GetSecondaryHashLocation, OutputResults
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: SearchChaining
+//  DESCRIPTION: function searches the separate chaining hash array to find half
+//               of the integers in the random array
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: randArray - reference array for random integer keys
+//                    chainingHash - reference array for separate chaining hash
+//                                   order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: GetPrimaryHashLocation, OutputResults
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: OutputResults
+//  DESCRIPTION: function will display search results for the user
+//  IMPLEMENTED BY:
+//  INPUT:
+//        Parameters: searchType - type of search executed (linear, double, or
+//                    chaining)
+//                    itemsExamined - integer of the items that were examined by
+//                    the search
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: None
+//***************************************************************************
+
+
+//***************************************************************************
+//  FUNCTION: DeleteNodes
+//  DESCRIPTION: function will delete chaining nodes to free up memory
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: chainingHash - reference array for separate chaining hash
+//                                   order of keys
+//  OUTPUT:
+//        Return Val: None
+//  CALLS TO: None
+//***************************************************************************
