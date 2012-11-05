077ed8b6b67ae3d848775b6b87a64c9d4c53d0cd
Jason Lopez <jlopez0896@gmail.com>
2012-11-04 19:18:16 -0700

    Add CreateChainingHash file

---
+++ Week3/Functions/CreateChainingHash.cpp
@@ -0,0 +1,19 @@
+
+
+//*****************************************************************************
+//  FUNCTION:    CreateChainingHash
+//  DESCRIPTION: function creates array of pointers to nodes. Array is of listSize
+//               and every index of array is initialized to NULL.
+//               (To be used as master array in Separate Chaining method)
+//  IMPLEMENTED BY: Scott Russell
+//  INPUT:
+//        Parameters: listSize - integer size of hash table
+//  OUTPUT:
+//        Return Val: chainingHash - array of pointers [listSize] large
+//  CALLS TO: None
+//******************************************************************************
+
+chainingHash** CreateChainingHash(int listSize)
+{
+
+}
